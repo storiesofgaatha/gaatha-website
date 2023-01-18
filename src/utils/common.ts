82c5dbbd4421b7f51a3b22d1cea10264b2b9fc07
barshathakuri <barshathakuri708@gmail.com>
2023-01-18 15:52:28 +0545

    Fix ports for server

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,6 @@
 const envVariables = {
-    graphqlEndpoint: process.env.NEXT_APP_GRAPHQL_ENDPOINT ?? 'http://server:8000/graphql/',
+    // FIXME: This has to be fixed
+    graphqlEndpoint: process.env.NEXT_APP_GRAPHQL_ENDPOINT ?? 'http://server:8020/graphql/',
 };
 
 export default envVariables;
