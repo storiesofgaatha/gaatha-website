4ddac51fbf1ebc7182a77bcf9df18bc131af190a
Aditya Khatri <adityakhatri47@gmail.com>
2023-07-06 10:14:27 +0545

    Fix height issue in mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import 'styles/globals.css';
 import 'styles/variables.css';
 
@@ -8,6 +9,16 @@ function MyApp(props: AppProps) {
         Component,
         pageProps,
     } = props;
+
+    useEffect(() => {
+        const documentHeight = () => {
+            const doc = document.documentElement;
+            doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
+        };
+        window.addEventListener('resize', documentHeight);
+        documentHeight();
+        return () => window.removeEventListener('resize', documentHeight);
+    }, []);
 
     return (
         // eslint-disable-next-line react/jsx-props-no-spreading
