938ef6a62d454bff8ae715097cd19d5e5276da25
Aditya Khatri <adityakhatri47@gmail.com>
2025-09-09 20:37:22 +0545

    fix: minor issues

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -5,8 +5,6 @@ import Link from 'next/link';
 
 import styles from './styles.module.css';
 
-const mapLink = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d883.3079914281183!2d85.31767726964863!3d27.679224598512448!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19e05ed0b465%3A0xecaf2661bf809d2d!2sGaatha!5e0!3m2!1sen!2snp!4v1684749192840!5m2!1sen!2snp';
-
 function Contact() {
     return (
         <PageWithSideBar
@@ -14,33 +12,7 @@ function Contact() {
             className={styles.page}
             contentClassName={styles.mainContent}
         >
-            <div className={styles.map}>
-                <div className={styles.mapOuter}>
-                    <iframe
-                        className={styles.gmapIframe}
-                        title="gaathaMap"
-                        id="gmap_canvas"
-                        src={mapLink}
-                    />
-                </div>
-            </div>
             <div className={styles.contact}>
-                <div className={styles.contactDetail}>
-                    <LabelValue
-                        className={styles.labelValue}
-                        label="Address"
-                        value={(
-                            <>
-                                <p>
-                                    Patan Dhoka - 03
-                                </p>
-                                <p>
-                                    Lalitpur, Province 3, Nepal
-                                </p>
-                            </>
-                        )}
-                    />
-                </div>
                 <div className={styles.contactDetail}>
                     <LabelValue
                         className={styles.labelValue}
